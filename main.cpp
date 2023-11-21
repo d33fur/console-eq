ab5cf9bd11fb18e2c76c392bbf1041dc11bfcddc
d33fur <d33fur@gmail.com>
2023-11-22 00:32:02 +0300

    some changes in CMakeLists.txt and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,104 +1,52 @@
-/*
-  ==============================================================================
+#include <iostream>
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <ncurses.h>
 
-    This file contains the basic startup code for a JUCE application.
+#define BUFSIZE 1024
 
-  ==============================================================================
-*/
+int main() {
+    pa_sample_spec ss;
+    ss.format = PA_SAMPLE_S16LE;
+    ss.channels = 1;
+    ss.rate = 44100;
+    pa_simple *s = NULL;
+    int error;
 
-#include "process.h"
-//==============================================================================
-class TEST1Application  : public juce::JUCEApplication
-{
-public:
-    //==============================================================================
-    TEST1Application() {}
-
-    const juce::String getApplicationName() override       { return "Preset Share"; }
-    const juce::String getApplicationVersion() override    { return "1.0.0"; }
-    bool moreThanOneInstanceAllowed() override             { return true; }
-
-    //==============================================================================
-    void initialise (const juce::String& commandLine) override
-    {
-        // This method is where you should put your application's initialisation code..
-
-        mainWindow.reset (new MainWindow (getApplicationName()));
+    if (!(s = pa_simple_new(NULL, "AudioSpectrogram", PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+        fprintf(stderr, "pa_simple_new() failed: %s\n", pa_strerror(error));
+        return -1;
     }
 
-    void shutdown() override
-    {
-        // Add your application's shutdown code here..
+    initscr();
+    timeout(0);
 
-        mainWindow = nullptr; // (deletes our window)
-    }
-
-    //==============================================================================
-    void systemRequestedQuit() override
-    {
-        // This is called when the app is being asked to quit: you can ignore this
-        // request and let the app carry on running, or call quit() to allow the app to close.
-        quit();
-    }
-
-    void anotherInstanceStarted (const juce::String& commandLine) override
-    {
-        // When another instance of the app is launched while this one is running,
-        // this method is invoked, and the commandLine parameter tells you what
-        // the other instance's command-line arguments were.
-    }
-
-    //==============================================================================
-    /*
-        This class implements the desktop window that contains an instance of
-        our MainComponent class.
-    */
-    class MainWindow    : public juce::DocumentWindow
-    {
-    public:
-        MainWindow (juce::String name)
-                : DocumentWindow (name,
-                                  juce::Desktop::getInstance().getDefaultLookAndFeel()
-                                          .findColour (juce::ResizableWindow::backgroundColourId),
-                                  DocumentWindow::allButtons)
-        {
-            setUsingNativeTitleBar (true);
-            setContentOwned (new MainComponent(), true);
-
-#if JUCE_IOS || JUCE_ANDROID
-            setFullScreen (true);
-#else
-            setResizable (true, true);
-            centreWithSize (getWidth(), getHeight());
-#endif
-
-            setVisible (true);
+    int16_t buf[BUFSIZE]; 
+    while (1) {
+        if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
+            fprintf(stderr, "pa_simple_read() failed: %s\n", pa_strerror(error));
+            break;
         }
 
-        void closeButtonPressed() override
-        {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
-            JUCEApplication::getInstance()->systemRequestedQuit();
+        clear();
+
+        for (int i = 0; i < BUFSIZE; i++) {
+            mvprintw(0, i, "%c", buf[i] > 0 ? '+' : '-');
         }
 
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
+        refresh();
 
-    private:
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
-    };
+        if (getch() == 'q') {
+            break;
+        }
+    }
 
-private:
 
-    std::unique_ptr<MainWindow> mainWindow;
-};
+    if (s) {
+        pa_simple_free(s);
+    }
 
-//==============================================================================
-// This macro generates the main() routine that launches the app.
-START_JUCE_APPLICATION (TEST1Application)
+    endwin();
+
+    return 0;
+}
